39d8939f88786cd03b8d6a3d3aaa2a1c071bf803
Adam Giese <adam.giese@underarmour.com>
2018-05-06 22:43:52 -0500

    Add initiation content
    Add The Domain content
    Add Umlauf Garden content
    Add Bastrop content
    Add Lockhart content

---
+++ src/js/content/austin.js
@@ -0,0 +1,13 @@
+export default const austin = [
+  {
+    "title": "The Domain",
+    "description": "Upscale retail and dining",
+    "link": "http://www.simon.com/mall/the-domain",
+    "completed": true,
+  },
+  {
+    "title": "Umlauf Sculpture Garden",
+    "link" : "http://www.umlaufsculpture.org/"
+    "completed": false,
+  }
+]
