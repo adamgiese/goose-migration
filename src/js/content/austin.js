2f633a16adabdb4bed02a7bd003a7fe5fb2c83ca
Adam Giese <adam.giese@underarmour.com>
2018-05-16 20:09:54 -0500

    Add 'Zilker Park' to austin activities

--- src/js/content/austin.js
+++ src/js/content/austin.js
@@ -41,5 +41,27 @@ const austin = [
       },
     ],
   },
+  {
+    title: 'Zilker Park',
+    completed: true,
+    link: 'http://austintexas.gov/department/zilker-metropolitan-park',
+    tags: [
+      'dog-friendly',
+      'outdoors',
+    ],
+    activities: [
+      {
+        title: 'Barton Springs Pool',
+        link: 'https://austintexas.gov/department/barton-springs-pool',
+      },
+      {
+        title: 'Zilker Botanical Garden',
+        link: 'http://austintexas.gov/content/zilker-botanical-garden',
+      },
+      {
+        title: 'Zilker Playscape',
+      },
+    ],
+  },
 ];
 export default austin;
