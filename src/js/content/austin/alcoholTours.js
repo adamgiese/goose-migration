20e156a429af3a1a671f7d51c6035bb893b22fdb
Adam Giese <adam.giese@gmail.com>
2018-05-23 19:51:50 -0500

    Add new breweries

--- src/js/content/austin/alcoholTours.js
+++ src/js/content/austin/alcoholTours.js
@@ -1,5 +1,20 @@
 const tours = [
   {
+    title: '(512) Brewing Company',
+    link: 'https://512brewing.com/',
+    tags: [
+      'alcohol',
+      'dog-friendly',
+    ],
+  },
+  {
+    title: 'Live Oak Brewing Company',
+    link: 'http://liveoakbrewing.com/',
+    tags: [
+      'alcohol',
+    ],
+  },
+  {
     title: 'Fitzhugh Trifecta',
     description: 'A brewery, cidery, and distillery.',
     completed: true,
